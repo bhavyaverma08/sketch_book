0585de115ba40df4b154923194e514c729705f42
bhavyaverma08 <verma100bhavya@gmail.com>
2024-05-02 15:30:46 +0530

    ADD react-painter template

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import "./App.css";
+import ReactPainter from "react-painter";
+
+function App() {
+  return (
+    <>
+      <ReactPainter
+        width={1000}
+        height={600}
+        render={({ canvas, triggerSave, setColor, setLineWidth, setLineCap, setLineJoin, imageDownloadUrl }) => (
+          <div>
+            <h1>Sketchbook</h1>
+            <div className="toolbox">
+              <div className="flex">
+                <label>Brush</label>
+                <input
+                  type="color"
+                  onChange={(e) => {
+                    setColor(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="flex">
+                <label>Brush Size</label>
+                <input
+                  type="range"
+                  min={"1"}
+                  max={"50"}
+                  onChange={(e) => {
+                    setLineWidth(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="flex">
+                <label>Line Join</label>
+                <select
+                  name=""
+                  id=""
+                  onChange={(e) => {
+                    setLineJoin(e.target.value);
+                  }}
+                >
+                  <option value="round">Round</option>
+                  <option value="bevel">Bevel</option>
+                  <option value="miter">Miter</option>
+                </select>
+              </div>
+              <div className="flex">
+                <label>Line Cap</label>
+                <select
+                  name=""
+                  id=""
+                  onChange={(e) => {
+                    setLineCap(e.target.value);
+                  }}
+                >
+                  <option value="round">Round</option>
+                  <option value="butt">Butt</option>
+                  <option value="sqaure">Square</option>
+                </select>
+              </div>
+              {imageDownloadUrl ? (
+                <a href={imageDownloadUrl}>Download</a>
+              ) : (
+                <button onClick={triggerSave}>Save sketch</button>
+              )}
+            </div>
+            <div className="awesomeContainer">{canvas}</div>
+          </div>
+        )}
+      />
+    </>
+  );
+}
+
+export default App;
